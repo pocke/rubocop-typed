5e53ac31ea09d85c7885b8b2b0b1f702485e87e8
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-11-02 12:43:59 +0900

    new cop debug

---
+++ spec/rubocop/cop/typed/debug_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Typed::Debug do
+  subject(:cop) { described_class.new(config) }
+
+  let(:config) { RuboCop::Config.new }
+
+  pending
+end
