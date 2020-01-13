d917aa04a1a8d051e0ec5d4357c604dc0a412b47
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2020-01-13 20:53:09 +0900

    WIP

--- lib/rubocop/cop/typed/dig.rb
+++ lib/rubocop/cop/typed/dig.rb
@@ -10,15 +10,17 @@ module RuboCop
           return unless sends
           return unless sends.size >= 2
 
-          type = RuboCop::Typed::Util.type_of_node(node: node.receiver, processed_source: processed_source)
-          interface = RuboCop::Typed::Util.type_to_interface(type: type, processed_source: processed_source)
-
-          binding.irb
+          respond_to_dig = sends.all? do |s|
+            type = RuboCop::Typed::Util.type_of_node(node: s.receiver, processed_source: processed_source)
+            interface = RuboCop::Typed::Util.type_to_interface(type: type, processed_source: processed_source)
+            interface.methods[:dig]
+          end
+          return unless respond_to_dig
 
           add_offense(node, message: sends.inspect)
         end
 
-        def sends(node)
+        private def sends(node)
           return unless node.send_type?
           return unless node.method_name == :[]
           return unless node.arguments.size == 1
